172b5725e5b81ed15fa3b4ba627af9b6b78d24c4
Nick Mealey <nick@heliumdev.com>
2016-07-22 16:36:44 -0700

    Users controllers and view setup.

--- app/controllers/comfy/admin/users_controller.rb
+++ app/controllers/comfy/admin/users_controller.rb
@@ -1,21 +1,17 @@
 class Comfy::Admin::UsersController < Comfy::Admin::Cms::BaseController
   def index
-    @users = User.all
+    @users = Comfy::User.all
     authorize! :read, :users
   end
 
   def show
-    authorize! :read, User
+    authorize! :read, Comfy::User
 
     if params[:id]
-      @user = User.find(params[:id])
+      @user = Comfy::User.find(params[:id])
     else
       @user = current_account
     end
-
-    @events = @user.events.paginate({
-      page: params[:page]
-    })
   rescue ActiveRecord::RecordNotFound
     flash[:alert] = "Couldn't find that user"
     redirect_to :back
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,7 +2,7 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    user ||= User.new(account_type: 'manager')
+    user ||= Comfy::User.new(account_type: 'manager')
 
     can :manage, :all
   end
