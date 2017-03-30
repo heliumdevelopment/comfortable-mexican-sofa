53ba3fba8022180e2c84a0743aad1e7bafa86679
Nick Mealey <nick@mealeydev.com>
2017-03-30 15:25:30 -0700

    Added full domain redirects.

--- app/controllers/comfy/admin/redirect_rules_controller.rb
+++ app/controllers/comfy/admin/redirect_rules_controller.rb
@@ -1,5 +1,5 @@
 class Comfy::Admin::RedirectRulesController < Comfy::Admin::Cms::BaseController
-  before_filter :authorize
+  before_filter :authorize, :set_path
 
   def index
     @redirects = RedirectRule.paginate(page: params[:page])
@@ -7,7 +7,11 @@ class Comfy::Admin::RedirectRulesController < Comfy::Admin::Cms::BaseController
 
   def new
     @redirect = RedirectRule.new
-    @path = "//" + request.host_with_port + "/"
+  end
+
+  def show
+    @redirect = RedirectRule.find(params[:id])
+    render 'edit'
   end
 
   def create
@@ -24,7 +28,6 @@ class Comfy::Admin::RedirectRulesController < Comfy::Admin::Cms::BaseController
 
   def edit
     @redirect = RedirectRule.find(params[:id])
-    @path = request.protocol + request.host + "/"
   end
 
   def update
@@ -32,12 +35,11 @@ class Comfy::Admin::RedirectRulesController < Comfy::Admin::Cms::BaseController
 
     if @redirect.update(redirect_params)
       flash[:notice] = "Updated Redirect"
-      redirect_to edit_comfy_redirect_rule_path(@redirect)
     else
       flash[:error] = "Ah snap! #{@redirect.display_errors}"
-      render 'edit'
     end
 
+    render 'edit'
   rescue ActiveRecord::RecordNotFound
     flash[:error] = "RedirectRule not found!"
     redirect_to :comfy_redirect_rules
@@ -61,6 +63,10 @@ class Comfy::Admin::RedirectRulesController < Comfy::Admin::Cms::BaseController
 
   private
 
+  def set_path
+    @path = request.host_with_port + "/"
+  end
+
   def authorize
     authorize! :manage, RedirectRule
   end
@@ -69,7 +75,6 @@ class Comfy::Admin::RedirectRulesController < Comfy::Admin::Cms::BaseController
     p = params.require(:redirect_rule).permit(:source, :destination, :active,
                         :source_is_regex)
     p[:source] = "/" + p[:source].gsub(/^\//,"")
-    p[:destination] = "/" + p[:destination].gsub(/^\//,"")
     p
   end
 end
